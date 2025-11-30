ae65d9fad77155a4414800697593165fe9bd88d8
Amith <amithajith7025@gmail.com>
2025-11-30 09:51:48 +0000

    Add/Updated file: 9.py

---
+++ 9.py
@@ -0,0 +1,9 @@
+def cat(str1, str2):
+	return str1 + str2
+
+str1 = input("Enter the string 1: ")
+str2 = input("Enter the string 2: ")
+
+str3 = cat(str1, str2)
+
+print(f"concatenated string: {str3}")
