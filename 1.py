56654e3dd0b07ceed14242b2490ac11e88a4f529
Amith <amithajith7025@gmail.com>
2025-11-30 09:51:48 +0000

    Add/Updated file: 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the number: "))
+flag = True
+
+for i in range(2, num//2 + 1):
+	if num % i == 0:
+		flag = False
+		break
+
+if flag:
+	print(f"{num} is a prime number.")
+else:
+	print(f"{num} is not a prime number.")
